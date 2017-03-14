eaa718a22fbf7d1c7bf729a3271a01e0175f9835
Konstantin <konstantin@live.no>
2017-03-14 11:12:04 +0100

    Add basic parser

--- interface.java
+++ interface.java
@@ -1,10 +1,21 @@
 class Interface{
+
+	static String[] parseInput(String input){
+		String parsedInput = input.toLowerCase();
+		String[] parsedInputArray = parsedInput.split(" ");
+		return parsedInputArray; 
+	}
+
 	public static void main(String[] args) {
 		while(true) {
-			System.out.print("What do you wish to do?");
-			System.out.print("");
+			String[] allowedCommands = {"help"};
+
+			System.out.print("What do you wish to do?" + System.lineSeparator() + ":");
 			String input = System.console().readLine();
-			System.out.print(input);
+			//System.out.print(input);
+			String[] commandArray = parseInput(input);
+			System.out.print(commandArray[0]);
+			System.out.print(commandArray[1]);
 		}
 	}
 }
